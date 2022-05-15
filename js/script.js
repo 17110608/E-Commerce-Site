3320450fd2876cdca44b8d9c37fffef8e2bec5d5
17110608 <vikramn06081999@gmail.com>
2022-05-15 08:58:01 +0530

    menu animation

--- js/script.js
+++ js/script.js
@@ -1,25 +1,19 @@
 const bar = document.getElementById("bar");
 const nav = document.getElementById("navbar");
+const cancel  = document.getElementById("cancel");
+
+cancel.onclick = noMenu;
 
 function menu() {
     nav.classList.add("active");
-    const cart = document.getElementById("cart");
-    const login = document.getElementById("login");
-
-    const ci = cart.innerHTML;
-    cart.innerHTML = ci + " Cart";
-    cart.style.fontWeight = "bold";
-
-    const li = login.innerHTML;
-    login.innerHTML = li + " Login";
-    login.style.fontWeight = "bold";
 }
 
 function noMenu() {
     nav.removeEventListener("mouseout", menu);
     nav.classList.remove("active");
-    console.log("Menu event stopped");
+    console.log("Menu event stopped");;
 }
+
 
 if (bar) {
     bar.addEventListener("click", function () {
