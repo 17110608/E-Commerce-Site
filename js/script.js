8390c4b04276647c952d3e6271e4d54c97851fb6
17110608 <vikramn06081999@gmail.com>
2022-05-15 11:50:41 +0530

    added all pages with basic design

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const bar = document.getElementById("bar");
 const nav = document.getElementById("navbar");
-const cancel  = document.getElementById("cancel");
+const cancel = document.getElementById("cancel");
+const login1 = document.getElementById("login1");
+const login2 = document.getElementById("login2");
+const hero = document.getElementById("hero");
+const login_container = document.getElementById("login-container");
 
 cancel.onclick = noMenu;
 
@@ -14,10 +18,40 @@ function noMenu() {
     console.log("Menu event stopped");;
 }
 
+function show_login() {
+    login_container.style = "display:block;";
+}
+
 
 if (bar) {
     bar.addEventListener("click", function () {
         menu();
+        login_container.style = "display:none";
         console.log("Menu event registered");
     });
 }
+
+if (login1 || login2) {
+    login1.addEventListener("click", function () {
+        show_login();
+        console.log("Login1 event registered");
+    });
+
+    login2.addEventListener("click", function () {
+        show_login();
+        console.log("Login2 event registered");
+    });
+}
+
+
+hero.addEventListener("mouseover", () => {
+    console.log("Hero Div clicked");
+    if (login1) {
+        login1.removeEventListener("click", show_login);
+    }
+    if (login2) {
+        login2.removeEventListener("click", show_login);
+    }
+    noMenu();
+    login_container.style = "display:none;"
+})
