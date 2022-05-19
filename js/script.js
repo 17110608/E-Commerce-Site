da5f96ad956030a7cc0b1efb6f3e7b407bf28340
Rinith-Amin003 <104890568+Rinith-Amin003@users.noreply.github.com>
2022-05-19 16:19:27 +0530

    show change

--- js/script.js
+++ js/script.js
@@ -188,10 +188,10 @@ function showSlides(n) {
         slideIndex = 1
     }
     if (n < 1) {
-        slideIndex = slides.length
+        slideIndex = slides.length;
     }
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        //slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
